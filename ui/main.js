153581cce0e85541278c442682153d42f3b0a832
pc121 <30957052+pc121@users.noreply.github.com>
2017-08-24 18:47:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,22 @@ var counter = 0;
 
 button.onclick = function(){
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function  ()
+    {
+      if(request.readyState === XMLHttpRequest.DONE){
+          if(request === 200){
+              //Teake action
+              var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();    
+          }
+      }
+      //What to do
+    };
+
+request.open('GET','http://chawarepappu.imad.hasura-app.io/counter',true);
+request.send(null);
+
 };
