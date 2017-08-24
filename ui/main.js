28d68fb3460c9c57fc548012a82f3cf53e4f9e4d
pc121 <30957052+pc121@users.noreply.github.com>
2017-08-24 19:12:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,26 @@ button.onclick = function(){
       //What to do
     };
 
+//make request
 request.open('GET','http://chawarepappu.imad.hasura-app.io/counter',true);
 request.send(null);
 
+};
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value; 
+var submit = document.getElementById('submit_btn');
+submit.onclick = function() {
+  //Make a request to server
+  
+  //Capture a list of names
+  var names = ['pappu','aheeto','dada'];
+  var list = '';
+  for(var i=0; i<names.length; i++){
+      list+= '<li>' + names[i] + '</li>';
+  }
+  var ul  = document.getElementById('list');
+  ul,innerHTML = list ;
+    
 };
