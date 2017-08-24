af5f90ffef19d6d6916ab3526b094467f0458d1e
pc121 <30957052+pc121@users.noreply.github.com>
2017-08-24 17:47:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ console.log('Loaded!');
 var btn = document.getElementById("btn");
 btn.onClick = function(){
     counter = counter+1;
-    var span = document.getElementById("btn");
+    var span = document.getElementById("count");
     span.innerHTML = counter.toString();
 }
