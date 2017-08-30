677cd0f67e340b3925b43063a7514b8094fca0d9
pc121 <30957052+pc121@users.noreply.github.com>
2017-08-30 19:32:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/articles/:articleName',function(req,res){
                 res.status(404).send(err.toString()+ ' Article Not Found');
             } else {
                 var articleData = result.rows[0];
-                res.send(createTemplate(articles[articleData]));
+                res.send(createTemplate(article[articleData]));
             }
             res.send(JSON.stringify(result.rows));
         }
