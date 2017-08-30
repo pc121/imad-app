30ed6579189f7903a1e8283401aa360438b60e33
pc121 <30957052+pc121@users.noreply.github.com>
2017-08-30 19:07:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('articles/:articleName',function(req,res){
    
     pool.query('SELECT * FROM articles WHERE title = ' + "'" +req.params.articleName + "'",function(err,result){
          if(err){
-            res.status(500).send(err.toString());
+            res.status(500).send(err.toString()+'law lag gye');
          } 
         else{
             if(result.rows.length === 0){
